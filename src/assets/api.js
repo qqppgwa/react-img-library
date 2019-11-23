c7a0c0f0dea4314c5d93e9e61ba5c529b24dfcaa
qqppgwa <qqppgwa@gmail.com>
2019-11-23 22:09:10 +0800

    load more button

--- src/assets/api.js
+++ src/assets/api.js
@@ -4,5 +4,5 @@ const getImg = axios.create({
     baseURL: 'https://api.giphy.com/v1'
 });
 // const searchPrefix='https://api.giphy.com/v1/gifs/search?api_key=IGLOYrMAV8KRjzGzj6vvF3dGx2WlASHZ&';
-export const apiSearch = data => getImg.get(`/gifs/search?api_key=${apiKey}&q=${data.query}&limit=${data.limit}`);
+export const apiSearch = data => getImg.get(`/gifs/search?api_key=${apiKey}&q=${data.query}&limit=${data.limit}&offset=${data.offset}`);
 export const apiImgId = data => getImg.get(`/gifs?api_key=${apiKey}&ids=${data.ids}`);
--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ImageCell from '../../components/ImageCell/ImageCell.jsx'
 import { apiSearch } from '../../assets/api'
+import styles from './index.module.css'
 class SearchPage extends React.Component {
   constructor() {
     super()
@@ -30,6 +31,11 @@ class SearchPage extends React.Component {
       <div>
         <InputArea keying={this.keying.bind(this)} changeStatus={this.changeStatus.bind(this)} list={this.list.bind(this)} state={this.state} />
         <div className="imgList">{this.state.status === '' ? null : s}</div>
+        {this.state.pagination && this.state.pagination.total_count - 8 > 0 && this.state.status === 'showlist' ? (
+          <button className={styles.more} onClick={this.nextPage.bind(this)}>
+            load more
+          </button>
+        ) : null}
       </div>
     )
   }
@@ -38,11 +44,37 @@ class SearchPage extends React.Component {
   }
   changeStatus(s) {
     this.setState({ status: s })
-    console.log(this.state)
   }
   list(l) {
-    this.setState({ list: l })
+    // let list = this.state
     console.log(this.state)
+    this.setState({ list: this.state.list.concat(l.data), pagination: l.pagination })
+    console.log(this.state)
+  }
+  nextPage() {
+    apiSearch({
+      query: this.state.keyword,
+      limit: '8',
+      offset: this.state.pagination.offset + 8
+    })
+      .then(res => {
+        console.log(res)
+        if (res.data.data.length > 0) {
+          // this.props.changeStatus('showlist')
+          console.log(res.data)
+
+          // this.setState({ status: 'showList' })
+        } else {
+          // this.props.changeStatus('none')
+          // this.setState({ status: 'null' })
+        }
+        this.list(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+        // this.props.changeStatus('error')
+        // this.setState({ status: 'error' })
+      })
   }
 }
 // class Results extends React.Component {}
@@ -51,7 +83,6 @@ class InputArea extends React.Component {
     return <input type="text" placeholder="Start searching for images!" onChange={this.keying.bind(this)} onKeyPress={this.search.bind(this)} value={this.props.state.keyword} />
   }
   keying(e) {
-    console.log(e.target.value)
     this.props.keying(e.target.value)
   }
   search(e) {
@@ -60,18 +91,20 @@ class InputArea extends React.Component {
       // this.setState({ status: 'loading' })
       apiSearch({
         query: this.props.state.keyword,
-        limit: '8'
+        limit: '8',
+        offset: '0'
       })
         .then(res => {
           console.log(res)
           if (res.data.data.length > 0) {
             this.props.changeStatus('showlist')
-            this.props.list(res.data.data)
+
             // this.setState({ status: 'showList' })
           } else {
             this.props.changeStatus('none')
             // this.setState({ status: 'null' })
           }
+          this.props.list(res.data)
         })
         .catch(err => {
           console.log(err)
